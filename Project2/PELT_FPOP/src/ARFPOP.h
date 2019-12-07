42c41123d863c71f0934dc222da3f22b9f6bb52d
Anuraag Srivastava <44287697+as4378@users.noreply.github.com>
2019-12-06 20:04:21 -0700

    Add files via upload

---
+++ Project2/PELT_FPOP/src/ARFPOP.h
@@ -0,0 +1,11 @@
+void ARFPOP
+(double *data_vec,
+  int data_count, double penalty, double gam, 
+  // the following matrices are for output.
+  double *cost_mat,
+  int *end_vec,
+  double *mean_vec,
+  int *intervals_mat, bool *constraint, 
+  int *success,
+  bool *compute_fitted_values, 
+  double EPS);
